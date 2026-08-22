350d708a27a11e71496582cba54f3ae631af8f15
Danger <dangerovidiu@yahoo.com>
2026-08-22 16:44:48 +0300

    -new update pre changes

--- Services/InfoClass.cs
+++ Services/InfoClass.cs
@@ -8,9 +8,9 @@ namespace UR_pnach_editor.Services
 {
     public class InfoClass
     {
-        public static string editorVersion = "5.6";
+        public static string editorVersion = "5.7";
         public static string fullEditorVersion = "Version " + editorVersion;
-        public static string editorReleaseDate = "Release Date 2/1/2026";
+        public static string editorReleaseDate = "Release Date 23/8/2026";
         public static string discordServer = "https://discord.gg/cwwJnteZEg";
         public static string youtubeLink = "https://www.youtube.com/playlist?list=PLSqKWicq9HZSQeolvsmjvXKR6S4ki2Eov";
         public static string tutorialLink = "https://www.youtube.com/watch?v=_seklkT9efs";
